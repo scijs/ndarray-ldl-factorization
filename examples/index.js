fdc062901edabf04302c3160a721fc6c43ca9d6f
Planeshifter <pburckhardt@outlook.com>
2015-05-02 14:14:04 -0400

    [UPDATE] implementation

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,12 @@
 'use strict';
 
-var foo = require( './../lib' );
+var ndarray = require('ndarray'),
+	show = require('ndarray-show'),
+	ldl = require( './../lib' );
+
+var A = ndarray(new Float64Array([9,-1,2,-1,8,-5,2,-5,7]), [3,3]);
+
+var ldl = ldl(A);
+console.log( 'A:\n' + show(A), '\n' );
+console.log( 'L:\n' + show(ldl.L) );
+console.log( 'd:\n' + show(ldl.d) );
